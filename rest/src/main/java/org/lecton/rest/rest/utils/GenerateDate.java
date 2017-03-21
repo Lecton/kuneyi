f1c7b8e9576061866e7b1022d38f0c0e383b15d9
Lecton <lectonlm@gmail.com>
2017-03-20 22:58:24 -0700

    set up

--- rest/src/main/java/org/lecton/rest/rest/utils/GenerateDate.java
+++
@@ -1,8 +0,0 @@
-package org.lecton.rest.rest.utils;
-
-public class GenerateDate {
-    public static String getCurrentTimeStamp(){
-        java.util.Date today = new java.util.Date();
-        return new java.sql.Timestamp(today.getTime()).toString();
-    }
-}
